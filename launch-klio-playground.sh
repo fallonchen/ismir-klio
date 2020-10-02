409642075328d504caa806294b4e9ec4064be5a3
Fallon Chen <fallon@spotify.com>
2020-10-02 19:02:12 +0200

    Add klio launcher

---
+++ launch-klio-playground.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -e $HOME=$(pwd) --workdir=$(pwd)  \
+           -v /var/run/docker.sock:/var/run/docker.sock \
+           --mount type=bind,source=$(pwd),target=$(pwd) \
+           -it \
+           --rm docker.io/fallon/ismir-klio 
