98779f011c16d94f037d9d4c3e19162707474fa6
Fallon Chen <fallon@spotify.com>
2020-10-07 18:28:52 +0200

    fix launcher script and add empty output directory

--- launch-klio-playground.sh
+++ launch-klio-playground.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -e $HOME=$(pwd) --workdir=$(pwd)  \
+docker run -e HOME=$(pwd) --workdir=$(pwd)  \
            -v /var/run/docker.sock:/var/run/docker.sock \
            --mount type=bind,source=$(pwd),target=$(pwd) \
            -it \
