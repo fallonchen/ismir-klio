4c490c4ad0fee5fd5c81e37ce36d59bc3cb43744
Fallon Chen <fallon@spotify.com>
2020-10-09 18:17:15 +0200

    remove unused imports in transforms.py and match slides

--- changed_files/transforms.py
+++ changed_files/transforms.py
@@ -4,13 +4,10 @@ Notice: the code below is just an example of what can be done.
 Feel free to import what's needed, including third-party libraries or
 other self-written modules.
 """
-import os
-import tempfile
-
 import apache_beam as beam
-from apache_beam.io.gcp import gcsio
 
 from klio.transforms import decorators
+
 from pitch_tracker.predictions import MyModel
 
 class HelloKlio(beam.DoFn):
@@ -19,22 +16,16 @@ class HelloKlio(beam.DoFn):
         self.model = MyModel()
 
     @decorators.handle_klio
-    def process(self, item):
-        """Any errors raised here (explicitly or not), Klio will catch and
-        log + drop the item.
+    def process(self, data):
+        element = data.element.decode("utf-8")
+        self._klio.logger.info(
+            "Received '%s' from file '%s'"
+            % (element, self._klio.config.job_config.events.inputs[0].file_pattern)
+        )
 
-        Args:
-            item (KlioMessage): The current workitem, containing both
-                the element reference, and the payload
-        Returns:
-            entity_id (str)
-        """
-        entity_id = item.element.decode("utf-8")
-        job_config_data = self._klio.config.job_config.data
+        job_config_data = self._klio.config.job_config.data 
         input_location = job_config_data.inputs[0].location
-        input_file = "{}/{}.wav".format(input_location, entity_id)
-        output_dir = job_config_data.outputs[0].location
-
-        print("input file: {}".format(input_file))
+        input_file = "{}/{}.wav".format(input_location, element)
+        output_dir = job_config_data.outputs[0].location        
         self.model.predict(input_file, output_dir)
-        yield item
+        yield data
